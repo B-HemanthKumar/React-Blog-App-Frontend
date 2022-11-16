ca5914b3a9ba9850f9618a0ab371f0f3df62262b
B-HemanthKumar <hemanthpandu328@gmail.com>
2022-11-16 18:39:18 +0530

    Changed browserrouter tag

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Route ,Routes} from "react-router-dom"
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL} >
       <Routes> 
         <Route path="/" element={< Home />} />
         <Route path="/bollywood" element={< Bollywood />} />
