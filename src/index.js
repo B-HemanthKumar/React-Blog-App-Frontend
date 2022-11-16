9725ea778884987dcdcd6555aa82f7b9b7d671ff
B-HemanthKumar <hemanthpandu328@gmail.com>
2022-11-16 19:26:47 +0530

    changed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 
@@ -8,9 +7,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <BrowserRouter basename={process.env.PUBLIC_URL} >
-       <App />
-     </BrowserRouter>
+    
+    <App />
+
    
   </React.StrictMode>
 );
