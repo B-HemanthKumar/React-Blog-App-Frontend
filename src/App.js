70212be11b1375eee098a987e95bcdeeeb9832e2
B-HemanthKumar <hemanthpandu328@gmail.com>
2022-11-16 18:53:41 +0530

    changed to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import Home from "./components/pages/home/Home";
 import Bollywood from "./components/pages/bollywood/Bollywood"
 import SinglePost from "./components/pages/singlepost/SinglePost";
 import React from 'react';
-import { HashRouter, Route ,Routes} from "react-router-dom"
+import { BrowserRouter, Route ,Routes} from "react-router-dom"
 
 
 function App() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL} >
+    <BrowserRouter basename={process.env.PUBLIC_URL} >
       <Routes> 
         <Route path="/" element={< Home />} />
         <Route path="/bollywood" element={< Bollywood />} />
         <Route path="/singlepost" element={< SinglePost />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   
   ) ;
 }
