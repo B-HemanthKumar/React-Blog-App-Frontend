6769574b5f88239460b20cc61b54964905358cc3
B-HemanthKumar <hemanthpandu328@gmail.com>
2022-11-16 19:43:49 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Route ,Routes} from "react-router-dom"
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL} >
+    <BrowserRouter >
       <Routes> 
         <Route path="/" element={< Home />} />
         <Route path="/bollywood" element={< Bollywood />} />
