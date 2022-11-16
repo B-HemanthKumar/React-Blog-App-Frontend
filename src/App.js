9922adb17c2726d197d380b1afeb00e0f297e862
B-HemanthKumar <hemanthpandu328@gmail.com>
2022-11-16 18:51:16 +0530

    Browserrouter chmaged to Hashrouter

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import Home from "./components/pages/home/Home";
 import Bollywood from "./components/pages/bollywood/Bollywood"
 import SinglePost from "./components/pages/singlepost/SinglePost";
 import React from 'react';
-import { BrowserRouter, Route ,Routes} from "react-router-dom"
+import { HashRouter, Route ,Routes} from "react-router-dom"
 
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL} >
+    <HashRouter basename={process.env.PUBLIC_URL} >
       <Routes> 
         <Route path="/" element={< Home />} />
         <Route path="/bollywood" element={< Bollywood />} />
         <Route path="/singlepost" element={< SinglePost />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   
   ) ;
 }
